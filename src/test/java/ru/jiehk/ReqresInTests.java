fdbb67f1a571c2a7be8b5a9e1b208b7d019df9b7
Elena Kosiakova <Elen.Nikitina6@x5.ru>
2023-07-20 18:30:22 +0200

    first commit

---
+++ src/test/java/ru/jiehk/ReqresInTests.java
@@ -0,0 +1,83 @@
+package ru.jiehk;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.*;
+
+public class ReqresInTests {
+    @Test
+    void registrationTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\" }";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"))
+                .body("id", is(4));
+    }
+
+    @Test
+    void missingPasswordRegistrationTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"\" }";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void missingEmailRegistrationTest() {
+        String data = "{ \"email\": \"\", \"password\": \"12345\" }";
+        given()
+                .log().all()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/register")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void usersListPaginationTest() {
+        given()
+                .log().all()
+                .param("page", 1)
+                .when()
+                .get("https://reqres.in/api/users")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data.id", hasSize(6))
+                .body("per_page", is(6));
+    }
+
+    @Test
+    void emptyUsersListTest() {
+        given()
+                .log().all()
+                .param("page", 100)
+                .when()
+                .get("https://reqres.in/api/users")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data", empty());
+    }
+
+}
